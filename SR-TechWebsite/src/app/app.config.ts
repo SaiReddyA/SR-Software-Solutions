9bdebcde7c1d58dd8bc35dc5c95f3e5e5dc6e685
Sai Reddy Local <saireddy1m43@gmail.com>
2025-05-18 18:01:38 +0530

    yash routing

--- SR-TechWebsite/src/app/app.config.ts
+++ SR-TechWebsite/src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
@@ -12,7 +12,7 @@ import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }),
-     provideRouter(routes), 
+     provideRouter(routes, withHashLocation()), 
      provideClientHydration(),
      provideAnimationsAsync()]
 };
